5669a3977081c70b9271f82d45606c1e2a4f03b4
smtronic <aasmolnikov@gmail.com>
2024-11-25 17:48:09 +0300

    feat(blog): add CommentForm for handling comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
+from dataclasses import fields
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -6,3 +8,9 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["name", "email", "body"]
