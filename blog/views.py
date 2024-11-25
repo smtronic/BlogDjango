7588ebe05bb032f98cf5ba1ef69eaa0cde34dba1
smtronic <aasmolnikov@gmail.com>
2024-11-25 18:33:39 +0300

    feat(blog): implement post_comment view and integrate comments in post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 from django.urls import is_valid_path
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
-from .forms import EmailPostForm
 from django.conf import settings
 from django.core.mail import send_mail
 from django.http import HttpRequest
@@ -36,10 +37,16 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+        },
     )
 
 
@@ -83,3 +90,28 @@ def post_share(request: HttpRequest, post_id):
         "blog/post/share.html",
         {"post": post, "form": form, "sent": sent},
     )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return render(
+            request,
+            "blog/post/comment.html",
+            {
+                "post": post,
+                "form": form,
+                "comment": comment,
+            },
+        )
