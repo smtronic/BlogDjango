dffcf7cdbe565d8548a5f101ae4b6e7a0ab702a9
smtronic <aasmolnikov@gmail.com>
2024-11-29 13:10:51 +0300

    Update settings and project dependencies; add mysite_data.json

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": os.getenv("DB_USER_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
