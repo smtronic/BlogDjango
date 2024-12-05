d59b5a10aa21bd2ea59740c465f5cccc8d4a381d
smtronic <aasmolnikov@gmail.com>
2024-12-05 18:12:19 +0300

    refactor(auth): reorganize templates and enhance authentication flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, CustomLoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path(
+        "login/",
+        CustomLoginView.as_view(
+            redirect_authenticated_user=True,
+            template_name="registration/login.html",
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LoginView.as_view(
+            template_name="registration/logout.html",
+        ),
+        name="logout",
+    ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,58 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from .forms import SignUpForm, LoginForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
+    initial = None  # принимает {'key': 'value'}
     template_name = "registration/signup.html"
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/")
+
+        return super(SignUpView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(
+            request,
+            self.template_name,
+            {"form": form},
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}")
+
+            return redirect("/")
+
+        return render(
+            request,
+            self.template_name,
+            {"form": form},
+        )
+
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get("remembert_me")
+
+        if not remember_me:
+            self.request.session.set_expiry(0)
+            self.request.session.modified = True
+
+        return super(CustomLoginView, self).form_valid(form)
