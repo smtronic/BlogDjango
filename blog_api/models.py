1ff809d1ba55cbce25deeef175c33dad2c9f0123
smtronic <aasmolnikov@gmail.com>
2024-12-17 18:12:53 +0300

    feat(api): initialize blog API app and add sample media files

---
+++ blog_api/models.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from blog.models import Comment
+
+# Create your models here.
+
+
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Comment(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.content = validated_data.get("content", instance.content)
+        instance.created = validated_data.get("created", instance.created)
+        return instance
---
+++ blog_api/views.py
@@ -0,0 +1,85 @@
+from functools import partial
+from django.http import Http404
+
+# Create your views here.
+from requests import delete
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
+
+from blog.models import Post
+from .serializers import PostSerializer
+from blog_api import serializers
+
+
+class UserPostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.kwargs["id"]
+        return Post.objects.filter(author=user)
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Post.objects.filter(author=user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# class PostList(APIView):
+#     def get(self, request, format=None):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(
+#             serializer.errors,
+#             status=status.HTTP_400_BAD_REQUEST,
+#         )
+
+
+# class PostDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def patch(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk, format=None):
+#         queryset = self.get_object(pk)
+#         queryset.delete()
+#         return Response(status.HTTP_204_NO_CONTENT)
