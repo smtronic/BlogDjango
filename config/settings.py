3fbd7dbd8f6b4a8ed49d8a89e85d062bf0d8ba0f
smtronic <aasmolnikov@gmail.com>
2024-12-19 17:20:21 +0300

    fix: move SECRET_KEY to environment variable, update dependencies

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z!s2$g(##y0s_8xnrxi=be8nhx(xly!0j^nc1*)_rtxfg%qx77"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
